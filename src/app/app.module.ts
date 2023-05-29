47ea662e95495d735ac96c6703229e6c4b15edd6
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-05-29 20:45:13 +0200

    feat: adding header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PageOneComponent } from './page-one/page-one.component';
 import { PageTwoComponent } from './page-two/page-two.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, PageOneComponent, PageTwoComponent],
+  declarations: [AppComponent, PageOneComponent, PageTwoComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
