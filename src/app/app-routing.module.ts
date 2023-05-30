b408f415c1ec075f652764a6137df7048a31c0c0
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-05-30 14:37:42 +0200

    feat: adding header, footer and basic routing (#2)
    * feat: adding two pages in routing
    
    * feat: adding header and footer
    
    * feat: improved routing, home page, components and pages separation
    
    * updated base route
    
    * updated angular.json
    
    * refresh build stuff..
    
    * build testing
    
    * build testing
    
    ---------
    
    Co-authored-by: mJarsater <martin.jarsater@hotmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PageOneComponent } from './pages/page-one/page-one.component';
+import { PageTwoComponent } from './pages/page-two/page-two.component';
+import { HomeComponent} from "./pages/home/home.component";
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Home aka start page
+  { path: 'page-one', component: PageOneComponent },
+  { path: 'page-two', component: PageTwoComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }, // Wildcard route
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
