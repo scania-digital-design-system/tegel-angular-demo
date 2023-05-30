2dd8deff1479be9ef711aa34dbc687200e027969
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-05-30 13:05:50 +0200

    feat: improved routing, home page, components and pages separation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PageOneComponent } from './page-one/page-one.component';
-import { PageTwoComponent } from './page-two/page-two.component';
+import { PageOneComponent } from './pages/page-one/page-one.component';
+import { PageTwoComponent } from './pages/page-two/page-two.component';
+import { HomeComponent} from "./pages/home/home.component";
 
 const routes: Routes = [
+  { path: '', component: HomeComponent }, // Home aka start page
   { path: 'page-one', component: PageOneComponent },
   { path: 'page-two', component: PageTwoComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }, // Wildcard route
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PageOneComponent } from './page-one/page-one.component';
-import { PageTwoComponent } from './page-two/page-two.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { PageOneComponent } from './pages/page-one/page-one.component';
+import { PageTwoComponent } from './pages/page-two/page-two.component';
+import { HeaderComponent} from './components/header/header.component';
+import { FooterComponent} from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, PageOneComponent, PageTwoComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, PageOneComponent, PageTwoComponent, HeaderComponent, FooterComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
