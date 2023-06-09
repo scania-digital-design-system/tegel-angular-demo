f01372e1f0b36cb2db27e61753d0dd46c0456e20
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-06-09 16:27:39 +0200

    docs(header): adding explanation comment

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  /* Here are two examples how to check current url to make "selected" prop of <tds-header-item> to work.
+   * Please check html file for more info */
+
   constructor(private router: Router) {}
   isActive(url: string): boolean {
     return this.router.isActive(url, true);
