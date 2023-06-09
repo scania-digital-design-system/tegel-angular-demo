c599c47c0f5f5b4026cd56f7595922c1bd7e8efa
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-06-09 16:08:00 +0200

    feat(header): two ways set selected status on item

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+  isActive(url: string): boolean {
+    return this.router.isActive(url, true);
+  }
+
+  getCurrentUrl(): string {
+    return this.router.url;
+  }
+}
