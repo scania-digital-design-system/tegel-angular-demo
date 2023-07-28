bf599e4ee5f881fce857d6273835261acc825c61
Tim <tim.romberg@scania.com>
2023-07-28 17:35:46 +0200

    feat: back to previous oruting syntax

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { PageTwoComponent } from './pages/page-two/page-two.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
-const routes: Routes = [
+const routes = [
   { path: '', component: HomeComponent }, // Home aka start page
 {
   path: 'form',
@@ -17,6 +17,6 @@ const routes: Routes = [
   { path: 'page-two', component: PageTwoComponent },
   { path: '404', component: ErrorPageComponent },
   { path: '**', redirectTo: '404', pathMatch: 'full' }, // Wildcard route
-];
+] satisfies Routes;
 
 export default routes;
