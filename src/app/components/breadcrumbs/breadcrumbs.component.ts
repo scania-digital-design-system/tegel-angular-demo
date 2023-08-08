5886f6c58a04826ada84933b55d4f941bedc0053
mJarsater <martin.jarsater@hotmail.com>
2023-08-08 11:46:15 +0200

    segmentation prepwork

--- src/app/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,12 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.css'],
+      imports: [
+        RouterLink,
+        RouterOutlet,
+    ],
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class BreadcrumbsComponent {
+    constructor(private router: Router) {}
+    segments = this.router.url.split('/').filter(Boolean);;
+
+
 
 }
