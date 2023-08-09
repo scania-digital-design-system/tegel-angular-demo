33b3167e258499b0f77359edd1668f47a1bbd147
mJarsater <martin.jarsater@hotmail.com>
2023-08-09 16:59:21 +0200

    futher form improvements

--- src/app/pages/form/slider/slider.component.ts
+++ src/app/pages/form/slider/slider.component.ts
@@ -1,14 +1,30 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SliderComponent {
 
+  @Input() sliderGroup : FormGroup;
+  @ViewChild('stressSlider', { static: true }) stressSlider!: ElementRef<HTMLTdsSliderElement>;
+  @ViewChild('balanceSlider', { static: true }) balanceSlider!: ElementRef<HTMLTdsSliderElement>;
+
+  handleChange(event:any, name: string){
+    this.sliderGroup.get(name)?.enable()
+    this.sliderGroup.get(name)?.setValue(event.detail.value)
+    if(name === 'happines'){
+      this.stressSlider.nativeElement.disabled = false;
+
+    } else if(name === 'stress'){
+      this.balanceSlider.nativeElement.disabled = false;
+
+    }
+  }
 }
