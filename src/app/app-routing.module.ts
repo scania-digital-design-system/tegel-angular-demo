49fb7f63517229a9aea0163113ce2eaab4a4813b
mJarsater <martin.jarsater@hotmail.com>
2023-08-11 14:56:59 +0200

    added subpage route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {  Routes } from '@angular/router';
 
 import { ComponentsComponent } from './pages/components/components.component';
 import { PageTwoComponent } from './pages/page-two/page-two.component';
@@ -9,6 +8,7 @@ import { TextPageComponent } from './pages/text-page/text-page.component';
 import { ChipsPageComponent } from './pages/chips-page/chips-page.component';
 import { TabsButtonsPageComponent } from './pages/tabs-buttons-page/tabs-buttons-page.component';
 import { TabsLinksPageComponent } from './pages/tabs-links-page/tabs-links-page.component';
+import { TextSubpageComponent } from './pages/text-subpage/text-subpage.component';
 
 const routes = [
   { path: '', component: HomeComponent }, // Home aka start page
@@ -18,7 +18,9 @@ const routes = [
 },
   { path: 'web-components', component: ComponentsComponent },
   { path: 'page-two', component: PageTwoComponent },
-  { path: 'text', component: TextPageComponent },
+  { path: 'text', component: TextPageComponent , children: [
+    { path: 'subpage', component: TextSubpageComponent }
+  ] },
   { path: 'chips', component: ChipsPageComponent },
   { path: 'tabs-buttons', component: TabsButtonsPageComponent },
   { path: 'tabs-links', component: TabsLinksPageComponent },
