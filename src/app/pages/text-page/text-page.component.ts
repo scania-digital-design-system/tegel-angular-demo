b4fa7bac0566dc25569a0b79b1bc9ec20b2218b1
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-08-14 18:34:17 +0200

    feat(link): integrate component (#20)
    * added subpage route
    
    * added subpage component
    
    * updated text page to include subpage

--- src/app/pages/text-page/text-page.component.ts
+++ src/app/pages/text-page/text-page.component.ts
@@ -1,5 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { BreadcrumbsComponent } from 'src/app/components/breadcrumbs/breadcrumbs.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-text-page',
@@ -7,8 +10,17 @@ import { BreadcrumbsComponent } from 'src/app/components/breadcrumbs/breadcrumbs
   styleUrls: ['./text-page.component.css'],
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [BreadcrumbsComponent],
+  imports: [BreadcrumbsComponent, RouterLink, RouterOutlet, CommonModule],
 })
 export class TextPageComponent {
+  constructor(private router: Router) {}
+
+  tegelStorybook = 'https://tegel-storybook.netlify.app/';
+
+  isTextRoute(): boolean {
+    
+    return this.router.url === '/text';
+  }
+
 
 }
