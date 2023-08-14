ceafeaaeffbc56e5baf3641d4e3077fe6343477b
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2023-08-14 17:36:38 +0200

    feat(badge): test

--- src/app/components/badge/badge.component.ts
+++ src/app/components/badge/badge.component.ts
@@ -1,4 +1,4 @@
-import { Component,  CUSTOM_ELEMENTS_SCHEMA, } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
 
 @Component({
   selector: 'app-badge',
@@ -7,7 +7,11 @@ import { Component,  CUSTOM_ELEMENTS_SCHEMA, } from '@angular/core';
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-
 export class BadgeComponent {
+  emailCount = 100;
 
+  updateEmailCount(event: any) {
+    // Assuming the value is stored as a string in event.target.value
+    this.emailCount = +event.target.value; // The + operator converts the string to a number
+  }
 }
