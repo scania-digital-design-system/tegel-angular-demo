3941c121ee73ebedc880fa5c7d549e9e1ce1b5c0
mJarsater <martin.jarsater@hotmail.com>
2023-08-16 10:57:23 +0200

    introduces simple state management for settings/side-menu/header

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,12 +1,54 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { UserStoreService } from 'src/app/services/user-store.service';
 
 @Component({
   selector: 'app-settings-page',
   standalone: true,
   templateUrl: './settings-page.component.html',
   styleUrls: ['./settings-page.component.css'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [ReactiveFormsModule,CommonModule],
 })
-export default class SettingsPageComponent {
+export default class SettingsPageComponent implements OnInit {
+  constructor(private userStoreService: UserStoreService) {}
 
+  userName = '';
+  userNameError = false;
+  placeOfWork = '';
+  placeOfWorkError = false;
+
+ 
+  
+  formField: FormGroup = new FormGroup({
+    userName: new FormControl('', Validators.minLength(10)),
+    placeOfWork: new FormControl('', Validators.required),
+  });
+
+
+  ngOnInit() {
+    this.userStoreService.userName$.subscribe((userName) => {
+      this.userName = userName;
+    });
+
+    this.userStoreService.placeOfWork$.subscribe((placeOfWork) => {
+      this.placeOfWork = placeOfWork;
+    });
+  }
+
+
+  submitForm(event: SubmitEvent){
+    event.preventDefault()
+    const form = event.target as HTMLFormElement
+
+    this.userNameError = this.formField.get('userName')?.value < 1
+    this.placeOfWorkError = this.formField.get('placeOfWork')?.value < 1
+    
+    if(!this.userNameError && !this.placeOfWorkError){
+      this.userStoreService.updateUserName(this.formField.get('userName')?.value)
+      this.userStoreService.updatePlaceOfWork(this.formField.get('placeOfWork')?.value)
+      form.reset()
+    }
+  }
 }
