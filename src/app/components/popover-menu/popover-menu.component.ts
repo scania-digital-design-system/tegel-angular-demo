6d8407cdd35f3e4dd524382dd25e9b3865287f77
Lindqvist Nathalie <nathalie.lindqvist@scania.com>
2023-08-16 17:07:21 +0200

    testing referenceEl

--- src/app/components/popover-menu/popover-menu.component.ts
+++ src/app/components/popover-menu/popover-menu.component.ts
@@ -1,25 +1,54 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, ViewChild } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 
+ 
+
 @Component({
+
   selector: 'app-popover-menu',
+
   standalone: true,
+
   imports: [CommonModule],
+
   templateUrl: './popover-menu.component.html',
+
   styleUrls: ['./popover-menu.component.css'],
+
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
 })
-export default class PopoverMenuComponent {
+
+export default class PopoverMenuComponent implements OnInit {
+
+
 
   toggleMenu = false;
 
-  @ViewChild('popoverMenu1', { static: true }) 
-  popoverMenu1!: ElementRef<HTMLElement>;
-  @ViewChild('trigger1', { static: true }) 
-  trigger1: ElementRef<HTMLElement>;
 
-  this.popovermenu1.referenceEl = trigger1;
+  @ViewChild('popoverMenu1', { static: true }) popoverMenu1!: ElementRef<HTMLElement>;
 
-  
+  @ViewChild('trigger1', { static: true }) trigger1: ElementRef<HTMLElement>;
 
+
+
+  ngOnInit(): void {
+
+    if(this.popoverMenu1 && this.trigger1){
+
+      //@ts-ignore
+
+      this.popoverMenu1.nativeElement.referenceEl = this.trigger1;
+
+    }
+
+    }
 }
+
+
+ 
+
+ 
+
+ 
