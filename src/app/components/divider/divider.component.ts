f093402047a5fa3f64c34659479621a42f590dfc
mJarsater <martin.jarsater@hotmail.com>
2023-08-17 09:19:21 +0200

    integrated divider component

---
+++ src/app/components/divider/divider.component.ts
@@ -0,0 +1,12 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-divider',
+  standalone: true,
+  templateUrl: './divider.component.html',
+  styleUrls: ['./divider.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class DividerComponent {
+
+}
