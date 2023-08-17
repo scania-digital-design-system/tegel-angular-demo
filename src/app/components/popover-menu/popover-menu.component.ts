50e44d8ebca88edd46e6b108531cdce6feccf97f
Lindqvist Nathalie <nathalie.lindqvist@scania.com>
2023-08-17 08:41:01 +0200

    remove referenceEl test code

--- src/app/components/popover-menu/popover-menu.component.ts
+++ src/app/components/popover-menu/popover-menu.component.ts
@@ -1,49 +1,19 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
 import { CommonModule } from '@angular/common';
 
- 
-
 @Component({
-
   selector: 'app-popover-menu',
-
   standalone: true,
-
   imports: [CommonModule],
-
   templateUrl: './popover-menu.component.html',
-
   styleUrls: ['./popover-menu.component.css'],
-
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
-
 })
 
-export default class PopoverMenuComponent implements OnInit {
-
-
-
+export default class PopoverMenuComponent {
+  
   toggleMenu = false;
 
-
-  @ViewChild('popoverMenu1', { static: true }) popoverMenu1!: ElementRef<HTMLElement>;
-
-  @ViewChild('trigger1', { static: true }) trigger1: ElementRef<HTMLElement>;
-
-
-
-  ngOnInit(): void {
-
-    if(this.popoverMenu1 && this.trigger1){
-
-      //@ts-ignore
-
-      this.popoverMenu1.nativeElement.referenceEl = this.trigger1;
-
-    }
-
-    }
 }
 
 
