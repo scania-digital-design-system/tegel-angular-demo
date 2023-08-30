ca63025f31ea6733e233b3fb8193c4d0adca5575
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-08-30 11:54:11 +0200

    fix(block): use only inherit-from-parent mode variant

--- src/app/components/block/block.component.ts
+++ src/app/components/block/block.component.ts
@@ -2,14 +2,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @Component({
   selector: 'app-block',
   templateUrl: './block.component.html',
-  styleUrls: ['./block.component.css'],
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class BlockComponent {
-  modeVariant = 'primary';
-
-  handleVariantChange(event: any) {
-    this.modeVariant = event.detail.value;
-  }
-}
+export class BlockComponent {}
