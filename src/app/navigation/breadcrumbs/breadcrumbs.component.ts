277410e67e98a6831d1b29766f86a52036e10464
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-09-04 15:02:10 +0200

    fix(navigation: breadcrumbs): unsubscribe on destroy

--- src/app/navigation/breadcrumbs/breadcrumbs.component.ts
+++ src/app/navigation/breadcrumbs/breadcrumbs.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnDestroy } from '@angular/core';
 import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { unslugify } from '../../../utils';
 import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'nav-breadcrumbs',
@@ -11,16 +12,23 @@ import { filter } from 'rxjs/operators';
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export default class BreadcrumbsNavComponent {
+export default class BreadcrumbsNavComponent implements OnDestroy {
   segments: Array<{ title: string; path: string }> = []; // Initialize an empty array
 
+  private navigationEndSubscription: Subscription | undefined;
   constructor(private router: Router) {
-    this.router.events
+    this.navigationEndSubscription = this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event: any) => {
         this.url = event.url;
         this.segments = this.generateSegments(this.url); // Initialize segments array here
       });
+  }
+
+  ngOnDestroy() {
+    if (this.navigationEndSubscription) {
+      this.navigationEndSubscription.unsubscribe();
+    }
   }
 
   generateSegments = (pathname: string): Array<{ title: string; path: string }> => {
