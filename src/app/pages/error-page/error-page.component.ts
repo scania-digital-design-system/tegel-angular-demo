1dc3cd5d4e35b39bf30bf4338ee300b0334efa6a
Tim <tim.romberg@scania.com>
2023-09-06 14:19:20 +0200

    feat: 404 page in place (#51)
    * feat: 404 page in place
    
    * fix: review feedback
    
    * flex-grow
    
    * fix: correct color for h2
    
    * fix: rabase fixes
    
    * fix: duplicate items in side-menu
    
    * fix: formatting
    
    ---------
    
    Co-authored-by: mJarsater <martin.jarsater@hotmail.com>
    Co-authored-by: theJohnnyMe <nikola.johnny.mirkovic@scania.com>

--- src/app/pages/error-page/error-page.component.ts
+++ src/app/pages/error-page/error-page.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-error-page',
-  templateUrl: './error-page.component.html',
-  standalone: true,
+    selector: 'app-error-page',
+    templateUrl: './error-page.component.html',
+    styleUrls: ['./error-page.component.css'],
+    standalone: true,
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
 })
-export default class ErrorPageComponent {}
+export default class ErrorPageComponent {
+
+}
