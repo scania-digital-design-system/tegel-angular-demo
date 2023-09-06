f10c6b78deeb9d8586d6858b26af1efad08234b5
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-09-06 14:53:45 +0200

    fix(modal): props name update

--- src/app/pages/table-page/table-modal.component.ts
+++ src/app/pages/table-page/table-modal.component.ts
@@ -1,30 +1,30 @@
-import {
-    CUSTOM_ELEMENTS_SCHEMA,
-    Component, Input,
-  } from '@angular/core'
-  @Component({
-    selector: 'app-table-modal',
-    template: `
-      <tds-modal
-        selector="#my-modal-button"
-        id="my-modal"
-        show
-        size="lg"
-        actions="static"
+import { CUSTOM_ELEMENTS_SCHEMA, Component, Input } from '@angular/core';
+@Component({
+  selector: 'app-table-modal',
+  template: `
+    <tds-modal
+      selector="#my-modal-button"
+      id="my-modal"
+      show
+      size="lg"
+      actions-position="static"
+    >
+      <h5
+        class="tds-modal-headline"
+        slot="header"
       >
-        <h5 class="tds-modal-headline" slot="header">
-          Batch Actions example
-        </h5>
-        <span slot="body">
+        Batch Actions example
+      </h5>
+      <span slot="body">
         <p>Downloaded data:</p>
-          {{content}}
-        </span>
-      </tds-modal>
-    `,
-    styles: [``],
-    standalone: true,
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
-  })
-  export class TableModalComponent {
-    @Input() content: string
-  }
+        {{ content }}
+      </span>
+    </tds-modal>
+  `,
+  styles: [``],
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class TableModalComponent {
+  @Input() content: string;
+}
