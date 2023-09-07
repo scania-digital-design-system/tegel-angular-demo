829c75c57cf65d74658d3ddc02964c3c25456e3f
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-09-07 11:35:58 +0200

    fix(mode-switchers): charlotte's review update

--- src/app/mode-switcher/mode-switcher.component.ts
+++ src/app/mode-switcher/mode-switcher.component.ts
@@ -9,7 +9,14 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component, EventEmitter, Output } from '@angula
 export class ModeSwitcherComponent {
   @Output() modeToggle = new EventEmitter<void>();
 
+  labelText: 'On' | 'Off' = 'On';
+
   toggleMode() {
     this.modeToggle.emit();
+    if (this.labelText === 'On') {
+      this.labelText = 'Off';
+    } else {
+      this.labelText = 'On';
+    }
   }
 }
