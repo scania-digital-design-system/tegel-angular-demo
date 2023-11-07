5e718c0ec7b739ee7a0fa4e9b3fdb89b91eac082
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-11-07 16:35:36 +0100

    chore: example of datetime value

--- src/app/components/datetime/datetime.component.ts
+++ src/app/components/datetime/datetime.component.ts
@@ -1,10 +1,16 @@
-import {  Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { TegelModule } from '@scania/tegel-angular';
 
 @Component({
   selector: 'app-datetime',
   standalone: true,
   templateUrl: './datetime.component.html',
-  imports: [ TegelModule]
+  imports: [TegelModule],
 })
-export default class DatetimeComponent {}
+export default class DatetimeComponent {
+  protected readonly event = event;
+
+  handleDatetimeChange(event: any) {
+    console.log('Selected value is:' + event.target.value);
+  }
+}
