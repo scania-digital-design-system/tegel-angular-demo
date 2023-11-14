5d6221335e245b3f27b77497a6b8d756f5137311
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-11-14 11:55:57 +0100

    feat: modal with table inside

--- src/app/pages/support-examples/modal-with-table/modal-with-table.component.ts
+++ src/app/pages/support-examples/modal-with-table/modal-with-table.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TegelModule } from '@scania/tegel-angular';
 import { CommonModule } from '@angular/common';
+import { PaginationTableComponent } from '@pages/table-page/pagination-table.component';
 
 @Component({
   selector: 'app-modal-with-table',
   templateUrl: './modal-with-table.component.html',
   standalone: true,
-  imports: [TegelModule, CommonModule],
+  imports: [TegelModule, CommonModule, PaginationTableComponent],
 })
 export default class ModalWithTableComponent {}
