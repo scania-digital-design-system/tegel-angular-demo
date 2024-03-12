51c765185beed38f3144584338823f3e5112d32c
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-03-12 16:38:45 +0100

    fix: basic test model

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,12 +1,22 @@
-import {  Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TegelModule } from '@scania/tegel-angular';
+import truckerData from '../../../assets/trucker-data.json';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
   templateUrl: './about-page.component.html',
   imports: [
-    TegelModule
+    TegelModule, CommonModule
   ]
 })
-export default class AboutPageComponent {}
+export default class AboutPageComponent implements OnInit {
+  tableRows = truckerData;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
