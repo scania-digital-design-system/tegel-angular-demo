03dff0891a9109d830893d94dc603308ee0533bb
Nathalie Lindqvist <nathalie.lindqvist@scania.com>
2024-07-03 14:15:44 +0200

    feat(ag-grid): change int values to strings for proper filtering functionality

--- src/app/pages/ag-grid/ag-grid.component.ts
+++ src/app/pages/ag-grid/ag-grid.component.ts
@@ -30,9 +30,9 @@ export default class AgGridComponent {
     filter: true
   };
   public rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 }
+    { make: 'Toyota', model: 'Celica', price: '35000' },
+    { make: 'Ford', model: 'Mondeo', price: '32000' },
+    { make: 'Porsche', model: 'Boxster', price: '72000' }
   ];
   public gridApi!: GridApi;
 
