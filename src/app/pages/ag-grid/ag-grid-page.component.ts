ed2189234affbb63cf684a7e9716aff57576d91c
Nathalie Lindqvist <nathalie.lindqvist@scania.com>
2024-07-12 09:07:20 +0200

    feat(ag-grid): add toggle for compact mode

--- src/app/pages/ag-grid/ag-grid-page.component.ts
+++ src/app/pages/ag-grid/ag-grid-page.component.ts
@@ -11,9 +11,31 @@ import {
   @Component({
     selector: 'app-table-page',
     template: `
+    <div class="tds-u-pb3">
+      <tds-toggle
+      (tdsToggle)="toggleMode()"
+      headline="Toggle compact mode"
+      size="sm"
+        >
+        <div slot="label">{{ labelText }}</div>
+      </tds-toggle>
+    </div>
+    <div class="tds-headline-02 tds-u-pb1">AG-Grid Column Search</div>
+    <p>This is an implementation of AG-Grid with column search.</p>
+    <div class="ag-theme-quartz" [class.compact]="classCompact" style="height: 500px; width: 100%;">
       <app-ag-grid-column-search></app-ag-grid-column-search>
+    </div>
+    <div class="tds-headline-02 tds-u-pb1 tds-u-pt3">AG-Grid Column Search Floating Filter</div>
+    <p>This is an implementation of AG-Grid with column search, including a floating text input for
+        filtering.</p>
+    <div class="ag-theme-quartz" [class.compact]="classCompact" style="height: 500px; width: 100%;">
       <app-ag-grid-column-search-floating-filter></app-ag-grid-column-search-floating-filter>
+    </div>
+    <div class="tds-headline-02 tds-u-pb1 tds-u-pt3">AG-Grid Editable Cells</div>
+    <p>This is an implementation of AG-Grid with editable cells.</p>
+    <div class="ag-theme-quartz" [class.compact]="classCompact" style="height: 500px; width: 100%;">
       <app-ag-grid-editable-cells></app-ag-grid-editable-cells>
+    </div>
     `,
     styles: [``],
     standalone: true,
@@ -25,4 +47,16 @@ import {
       TegelModule
     ],
   })
-  export default class AgGridPageComponent {}
+  export default class AgGridPageComponent {
+    classCompact = false;
+    labelText: 'Compact' | 'Default' = 'Default';
+
+    public toggleMode() {
+      this.classCompact = !this.classCompact;
+      if (this.labelText === 'Compact') {
+        this.labelText = 'Default';
+      } else {
+        this.labelText = 'Compact';
+      }
+    }
+  }
