3b7079159ce19a191e28130f553315a0152f521c
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-08-06 14:13:40 +0200

    chore: testing

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+
 import { TegelModule } from '@scania/tegel-angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabs',
@@ -23,20 +25,18 @@ import { TegelModule } from '@scania/tegel-angular';
     </tds-folder-tabs>
 
     <div class="tds-headline-03 tds-u-pb3 tds-u-pt3">Inline</div>
-    <tds-inline-tabs>
-      <tds-inline-tab>
-        <button>First tab</button>
-      </tds-inline-tab>
-      <tds-inline-tab>
-        <button>Second tab is much longer</button>
-      </tds-inline-tab>
-      <tds-inline-tab>
-        <button>Third tab</button>
-      </tds-inline-tab>
-      <tds-inline-tab disabled>
-        <button>Fourth tab</button>
+    <tds-inline-tabs
+      *ngIf="loaded"
+      id="inline-tabs"
+      #tabsWrapper
+    >
+      <tds-inline-tab *ngFor="let tab of inlineTabs">
+        <button>{{ tab }}</button>
       </tds-inline-tab>
     </tds-inline-tabs>
+    <button (click)="injectInlineTab()">Inject inline tabs</button>
+    <button (click)="injectSingleTab()">Inject single tab</button>
+    <button (click)="setDefaultSelectedIndex()">Set default selected index</button>
 
     <div class="tds-headline-03 tds-u-pb3 tds-u-pt3">Navigation</div>
     <tds-navigation-tabs>
@@ -54,6 +54,34 @@ import { TegelModule } from '@scania/tegel-angular';
       </tds-navigation-tab>
     </tds-navigation-tabs>
   `,
-  imports: [TegelModule]
+  imports: [TegelModule, CommonModule],
 })
-export class TabsComponent {}
+export class TabsComponent implements AfterViewInit {
+  @ViewChild('tabsWrapper') tabsWrapper: ElementRef;
+  loaded = false;
+  selectedIndex = 0;
+  inlineTabs: string[] = ['First tab', 'Second tab is much longer', 'Third tab', 'Fourth tab'];
+
+  ngAfterViewInit() {
+    // Ensure the element is available after view initialization
+    if (this.tabsWrapper) {
+      this.tabsWrapper.nativeElement.defaultSelectedIndex = this.selectedIndex;
+    }
+  }
+
+  injectInlineTab() {
+    this.loaded = true;
+  }
+
+  injectSingleTab() {
+    //Add new tab to the end of the list
+    this.inlineTabs.push(`${this.inlineTabs.length} tab`);
+  }
+
+  setDefaultSelectedIndex() {
+    if (this.tabsWrapper) {
+      const tabsWrapper = this.tabsWrapper.nativeElement;
+      tabsWrapper.defaultSelectedIndex = this.selectedIndex;
+    }
+  }
+}
