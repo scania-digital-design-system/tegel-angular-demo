e79b79caaf0deefd5b957cd23ba9d063dbc1c0a7
Charles Krook <charles.krook@gmail.com>
2025-07-09 11:25:52 +0200

    feat: update @scania/tegel-angular to 1.32.2-dropdown-qa-beta.0 and enhance AboutPageComponent with dropdown handling

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,12 +1,27 @@
-import {  Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { TegelModule } from '@scania/tegel-angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
   templateUrl: './about-page.component.html',
-  imports: [
-    TegelModule
-  ]
+  imports: [TegelModule, CommonModule],
 })
-export default class AboutPageComponent {}
+export default class AboutPageComponent {
+  selectedValues: { [key: string]: any } = {};
+
+  handleDropdownChange(event: any, dropdownName: string) {
+    this.selectedValues = {
+      ...this.selectedValues,
+      [dropdownName]: event.detail.value,
+    };
+  }
+
+  getSelectedDisplay(value: any): string {
+    if (Array.isArray(value)) {
+      return value.join(', ');
+    }
+    return value || 'None';
+  }
+}
